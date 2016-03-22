520b2ad0f5488e6f81cd3eff353b2bde62785adb
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-22 10:26:32 -0300

    organizing some main files

--- gulpfile.js
+++ gulpfile.js
@@ -12,12 +12,12 @@ var gulp = require('gulp')
   , nib = require('nib')
   , header = require('gulp-header')
   , notify = require('gulp-notify')
-  , ghPages = require('gulp-gh-pages');
+  , ghPages = require('gulp-gh-pages')
+;
 
 var config = {
   port: 3000,
   devBaseUrl: 'http://localhost',
-
   paths: {
     html: './src/*'
     , css: [
@@ -39,19 +39,22 @@ gulp.task('connect', () => {
 });
 
 gulp.task('open', ['connect'], () => {
-  gulp.src('./index.html')
+  gulp
+    .src('./index.html')
     .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
 });
 
 gulp.task('html', () => {
-  gulp.src(config.paths.html)
+  gulp
+    .src(config.paths.html)
     .pipe(gulp.dest(config.paths.dist))
     .pipe(connect.reload())
     .pipe(notify('HTML OK!'));
 });
 
 gulp.task('css', () => {
-  gulp.src(config.paths.css)
+  gulp
+    .src(config.paths.css)
     .pipe(stylus({ use: nib(), compress: true }))
     .pipe(concat('bundle.css'))
     .pipe(connect.reload())
@@ -60,7 +63,8 @@ gulp.task('css', () => {
 });
 
 gulp.task('images', () => {
-  return gulp.src(config.paths.img)
+  return gulp
+    .src(config.paths.img)
     .pipe(imagemin({
       progressive: true,
       svgoPlugins: [{removeViewBox: false}],
@@ -70,7 +74,8 @@ gulp.task('images', () => {
 });
 
 gulp.task('fonts', () => {
-  return gulp.src([config.paths.fonts])
+  return gulp
+    .src([config.paths.fonts])
     .pipe(gulp.dest(config.paths.dist + '/assets/fonts'));
 });
 
@@ -79,11 +84,4 @@ gulp.task('watch', () => {
   gulp.watch(config.paths.css, ['css']);
 });
 
-gulp.task ('default',[
-  'html'
-  , 'images'
-  , 'fonts'
-  , 'css'
-  , 'open'
-  , 'watch'
-  ]);
+gulp.task ('default',['html', 'images', 'fonts', 'css', 'open', 'watch']);
